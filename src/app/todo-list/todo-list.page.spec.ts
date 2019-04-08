64407546640d922d6bad5264c9dc2ab3e2e7be95
John Sangiolo <johnsangiolo@gmail.com>
2019-04-08 15:35:46 -0400

    replaced home page with todo-list page

--- src/app/todo-list/todo-list.page.spec.ts
+++ src/app/todo-list/todo-list.page.spec.ts
@@ -1,22 +1,22 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomePage } from './home.page';
+import { TodoListPage } from './todo-list.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('TodoListPage', () => {
+  let component: TodoListPage;
+  let fixture: ComponentFixture<TodoListPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ TodoListPage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     })
-      .compileComponents();
+    .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(TodoListPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
